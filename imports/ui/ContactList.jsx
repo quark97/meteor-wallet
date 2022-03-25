0e59e9141b9c39f44d8a3ca0cb4e32ea30d1a1a6
quark97 <quark97@gmail.com>
2022-03-25 12:55:47 -0300

    Primer Commit

---
+++ imports/ui/ContactList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { ContactForm } from "./ContactForm";
+import { ContactsCollection } from "../api/ContactsCollection";
+import {useTracker} from 'meteor/react-meteor-data';
+
+export const ContactList = () => {
+    
+    const contacts = useTracker(() => {
+        return ContactsCollection.find({}).fetch();
+    })
+
+    return (
+                <>
+                   <h3>Contact List</h3>
+                   {contacts.map(contact => (
+                       <li key={contact.email}>{contact.name} - {contact.email}</li>
+                   ))
+
+                   }
+                </>
+            )
+
+    }
---
+++ server/main.js
@@ -0,0 +1,7 @@
+import { Meteor } from 'meteor/meteor';
+import "../imports/api/ContactsCollection";
+
+
+Meteor.startup(() => {
+  // If the Links collection is empty, add some data.
+});
